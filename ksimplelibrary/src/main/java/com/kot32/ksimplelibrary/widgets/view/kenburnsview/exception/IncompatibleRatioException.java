9274b5c04eef1ece1fe6ff9a968d2ceeb46a8f54
LeonidasCl <476020050@qq.com>
2017-02-27 15:11:53 +0800

    initial commit

--- ksimplelibrary/src/main/java/com/kot32/ksimplelibrary/widgets/view/kenburnsview/exception/IncompatibleRatioException.java
+++
@@ -1,8 +0,0 @@
-package com.kot32.ksimplelibrary.widgets.view.kenburnsview.exception;
-
-public class IncompatibleRatioException extends RuntimeException {
-
-    public IncompatibleRatioException() {
-        super("Can't perform Ken Burns effect on rects with distinct aspect ratios!");
-    }
-}
